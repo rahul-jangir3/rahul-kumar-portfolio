1bac966aa5e9b7a7296fc8bdaaf3fb66d26de159
Rahul Jangir <118007501+rahul-jangir3@users.noreply.github.com>
2025-08-22 17:07:36 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,10 +8,7 @@ const experiences = [
     role: 'Python Developer',
     company: 'Kanak Infosystems LLP',
     duration: 'March 2022 - May 2023',
-    description: `• Developed Apple login integration app for Odoo ERP.
-                  • Configured Docker containers for local and staging environments to streamline development and testing.
-                  • Configured client-server communication setup to Odoo custom app to client systems using REST APIs.
-                  • Handled end-to-end client deployment lifecycle, including module packaging, version control, and production server deployment.`
+    description: "• Developed Apple login integration app for Odoo ERP.\n• Configured Docker containers for local and staging environments to streamline development and testing.\n• Configured client-server communication setup to Odoo custom app to client systems using REST APIs.\n• Handled end-to-end client deployment lifecycle, including module packaging, version control, and production server deployment."
   {
     role: 'XXXXXXXXXX',
     company: 'XXXXXXXXXXXXX',
