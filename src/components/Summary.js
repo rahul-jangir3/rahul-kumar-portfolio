0fc0ab4e684306ae37c21226036c42fdc30628ce
Rahul Jangir <118007501+rahul-jangir3@users.noreply.github.com>
2025-08-22 16:57:45 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -14,7 +14,7 @@ function Summary() {
     >
       <h2 className="section-title mb-4">Summary</h2>
       <p className="summary-text lead">
-       XXXXXXXXXXXXX
+       Passionate AWS & DevOps Engineer with a strong foundation in Python development and hands-on expertise in cloud computing, automation, and CI/CD pipelines. Experienced in working with AWS services (EC2, S3, RDS, IAM, VPC, Lambda, CloudFormation) and DevOps tools (Docker, Kubernetes, Terraform, Ansible, Jenkins, GitHub Actions) to design scalable, secure, and efficient cloud solutions. Proven ability to automate infrastructure using IaC, streamline deployments with CI/CD pipelines, and monitor systems with modern observability tools. Transitioned from Python | Odoo development into cloud and DevOps, backed by real-world projects in containerization, Kubernetes, and automation pipelines. Continuously learning, contributing to projects, and eager to leverage expertise in driving cloud-native transformations.
       </p>
     </motion.section>
   );
