de3a18bff94601061e64334f3ea38eec42c86cd8
Rahul Jangir <118007501+rahul-jangir3@users.noreply.github.com>
2025-08-22 16:37:38 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,40 +24,40 @@ function Contact() {
               <div className="card-body text-start">
                 <p className="contact-info mb-3">
                   <FaEnvelope className="me-2 icon-warning" />
-                  <a href="mailto:avijitgorai8250@gmail.com">XXXXXXXXXXXXXXXX@gmail.com</a>
+                  <a href="mailto:avijitgorai8250@gmail.com">j.rahuljangir6@gmail.com</a>
                 </p>
                 <p className="contact-info mb-3">
                   <FaPhoneAlt className="me-2 icon-warning" />
-                  <a href="tel:+91 9391934987">+91 XXXXXXXXXXXXXX</a>
+                  <a href="tel:+91 9391934987">+91 6377399913</a>
                 </p>
                 <p className="contact-info mb-3">
                   <FaMapMarkerAlt className="me-2 icon-warning" />
-                  XXXXXXX, XXStateXXXX, India
+                  Delhi, India
                 </p>
                 <p className="contact-info mb-3">
                   <FaLinkedin className="me-2 icon-warning" />
                   <a
-                    href="https://www.linkedin.com/in/XXXXXXXXX/"
+                    href="https://www.linkedin.com/in/rahul-jangir-devops/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    linkedin.com/in/XXXXXXXXX 
+                    linkedin.com/in/rahul-jangir-devops
                   </a>
                 </p>
 		<p className="contact-info mb-3">
                   <FaGithub className="me-2 icon-warning" />
                   <a
-                    href="https://github.com/XXXXXXX/XXXXX.git"
+                    href="https://github.com/rahul-jangir3"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    GitHub Repository
+                    GitHub
                   </a>
                 </p>
                 <p className="contact-info">
                   <FaWhatsapp className="me-2 icon-warning" />
                   <a
-                    href="https://wa.me/91XXXXXXXX"
+                    href="https://wa.me/918696979378"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
